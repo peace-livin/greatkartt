7756b7007607944b8353122ed7063002e64dcca6
Akshay Darade <akshaydarade6675@gmail.com>
2025-10-06 14:28:06 +0000

    adding category  and superuser

--- greatkart/testapp/models.py
+++ greatkart/testapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100,unique=True)
+    Category_slug = models.SlugField(max_length=100,unique=True)
+    description = models.TextField(max_length=1000, blank=True)
+    cat_image = models.ImageField(upload_to='images/categories/', blank=True)
+    
+    def __str__(self):
+        return self.category_name
