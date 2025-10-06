5de139d4d21798e8c494ffc20ccc702e48df40c0
Akshay Darade <akshaydarade6675@gmail.com>
2025-10-06 15:24:51 +0000

    custome user model

--- greatkart/accounts/models.py
+++ greatkart/accounts/models.py
@@ -1,60 +1,65 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, phone_number,username, password=None):
+    def create_user(self, email, first_name, last_name, phone_number, username, password=None):
         if not email:
             raise ValueError('Users must have an email address')
-        
         if not username:
             raise ValueError('Users must have a username')
-        
+        if not phone_number:
+            raise ValueError('Users must have a phone number')
+
         user = self.model(
             email=self.normalize_email(email),
             first_name=first_name,
             last_name=last_name,
-            
+            username=username,
+            phone_number=phone_number,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
-    def create_superuser(self, email, first_name, last_name,username, password):
+
+    def create_superuser(self, email, first_name, last_name, phone_number, username, password=None):
         user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
+            email=email,
             first_name=first_name,
             last_name=last_name,
+            phone_number=phone_number,
+            username=username,
+            password=password,
         )
         user.is_admin = True
         user.is_staff = True
         user.is_active = True
-        user.is_superuser = True
+        user.is_superuser = True  # Required by Django's admin
         user.save(using=self._db)
         return user
-        
-        
-class Account(AbstractBaseUser):
+
+
+class Account(AbstractBaseUser, PermissionsMixin):  # Add PermissionsMixin for admin permissions
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=100)
-    
-    
+
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
-    
+    is_superuser = models.BooleanField(default=False)  # Changed from is_superadmin
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [ 'username','first_name', 'last_name']
-    
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
+
+    objects = MyAccountManager()
+
     def __str__(self):
         return self.email
-    
+
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "greatkart.wsgi.application"
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
