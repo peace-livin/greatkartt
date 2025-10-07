3819105b78e1f06caf8e50a3280bf063ff605c52
Akshay Darade <akshaydarade6675@gmail.com>
2025-10-07 11:58:18 +0000

    display product on homepage

--- greatkart/testapp/views.py
+++ greatkart/testapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from storee.models import Product
 # Create your views here.
 
 def home(request):
-    return render(request, 'testapp/home.html')
+    products = Product.objects.all().filter(is_available=True)
+    
+    context ={
+        'products':products
+    }
+    return render(request,'testapp/home.html',context)
